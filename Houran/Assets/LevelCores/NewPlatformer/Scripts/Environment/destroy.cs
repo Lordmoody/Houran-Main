4228338f78b68f70467015faa5e677f93799afc5
mrmfk <50992240+mrmfk@users.noreply.github.com>
2022-10-22 20:24:04 +0330

    mfk update4
    -level 8 added
    -destroy code added

---
+++ Houran/Assets/LevelCores/NewPlatformer/Scripts/Environment/destroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+    public  GameObject des;
+  
+    public  void dest()
+    {
+        des.SetActive(false);
+    }
+}
