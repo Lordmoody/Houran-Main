9f87ada12daf79f8628263eb7b0461ef850bb796
sobhan-hz <sobhanhaghighizadeh@yahoo.com>
2023-03-20 01:15:29 +0330

    sobhanupdate11
    bug fixes

--- Houran/Assets/LevelCores/NewPlatformer/Scripts/Environment/destroy.cs
+++ Houran/Assets/LevelCores/NewPlatformer/Scripts/Environment/destroy.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+
+
 
 public class destroy : MonoBehaviour
 {
-    public  GameObject des;
+        public  GameObject des;
+        [SerializeField]
+        private CinemachineVirtualCamera vcam1;
   
     public  void dest()
     {
         des.SetActive(false);
+        vcam1.Priority = 15;
+        Invoke("cameraSwitch" , 5);
+    }
+
+    public void cameraSwitch(){
+        vcam1.Priority = 8;
+
     }
 }
