b30858b918826772327a1a897151e48c2a014f17
sobhan-hz <sobhanhaghighizadeh@yahoo.com>
2023-03-29 19:40:37 +0430

    sobhan Update
    menu button fix

--- Houran/Assets/LevelCores/NewPlatformer/Scripts/Menu/MenuControllerNew.cs
+++ Houran/Assets/LevelCores/NewPlatformer/Scripts/Menu/MenuControllerNew.cs
@@ -6,12 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuControllerNew : MonoBehaviour
 {
+    public AudioSource audio;
 
     public void PlayScene(){
+        audio.Play();
         SceneManager.LoadScene("TestMovement");
     }
 
     public void Exit(){
+        audio.Play();
         Application.Quit();
     }
 }
